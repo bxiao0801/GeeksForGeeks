57233937a63c87a8d4bee3979b91e4fafd286995
Bingjie Xiao <bxiao0801@gmail.com>
2016-04-09 23:32:57 -0400

    Create Longest1'sByTurning0To1.java

---
+++ Longest1'sByTurning0To1.java
@@ -0,0 +1,25 @@
+/*Given an array of 0s and 1s, find the position of 0 to be replaced with 1 to get longest continuous sequence of 1s. 
+**Expected time complexity is O(n) and auxiliary space is O(1).
+*/
+public class Solution{
+	public int maxLen(int[] nums){
+		int maxLen=0;
+		int left=0;
+		int i=-1;
+		int j=0;
+		int index=Integer.MAX_VALUE;
+		while(i<nums.length){
+			while(j<nums.length&&nums[j]==1){
+				j++;
+			}
+			if(left+j-i>maxLen){
+				index=i;
+				maxLen=left+j-i;
+			}
+			left=j-i-1;
+			i=j;
+			j++;
+		}
+		return index;
+	}
+}
